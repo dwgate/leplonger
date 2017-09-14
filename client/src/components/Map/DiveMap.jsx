5d46d88b5681909832748aa6710adc88344c22fe
jiaconis <john.t.iaconis@gmail.com>
2017-09-13 22:57:06 -0700

    Greyscale map style added and background img removed 9/13 11pm

--- client/src/components/Map/DiveMap.jsx
+++ client/src/components/Map/DiveMap.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
 import MarkerContainer from './MarkerContainer.jsx';
-// import styles from './mapStyles.json';
+import styles from './mapStyles.json';
 
 const DiveMap = withGoogleMap(props => (
   <GoogleMap
-  // defaultOptions={{styles: styles}}
-    mapTypeId={google.maps.MapTypeId.SATELLITE}
+    defaultOptions={{ styles }}
+    // mapTypeId={google.maps.MapTypeId.SATELLITE}
     ref={props.onMapLoad}
     defaultZoom={6}
-    defaultOptions={{ scrollwheel: false }}
+    // defaultOptions={{ scrollwheel: false }}
     defaultCenter={{ lat: 37.787, lng: -122.458 }}
   >
     {props.markers.map((marker, index) => {
